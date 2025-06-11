e2e8874366e335357730bece8e6406456911dae3
Fadl711 <fathlvvee@gmail.com>
2025-06-11 22:04:53 +0300

    auth

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -13,8 +13,8 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/dashboard',
     },
 ];
-export default function Home() {
-    
+export default function Home({auth}) {
+
 
     const { props } = usePage();
     const { recipes, categorys, ratings, blogs_all, tags } = props;
@@ -39,36 +39,67 @@ export default function Home() {
     };
 
     return (
- <AppLayout breadcrumbs={breadcrumbs}>
-                <Head title="Home" />
-            <div className="w-full" ref={topRef}>
-                <h5 className="text-2xl tracking-tight">Your passion for food starts here!</h5>
+        <>
+            {auth.user && (
+                <AppLayout breadcrumbs={breadcrumbs}>
+                    <Head title="Home" />
+                    <div className="w-full" ref={topRef}>
+                        <h5 className="text-2xl tracking-tight">Your passion for food starts here!</h5>
 
-                <a href="#" className="flex flex-col max-md:flex-row md:flex-row">
-                    <div className="flex flex-col justify-between px-1 py-4">
-                        <h4 className="text-1xl"> Best food recipes with the foodie v network </h4>
+                        <a href="#" className="flex flex-col max-md:flex-row md:flex-row">
+                            <div className="flex flex-col justify-between px-1 py-4">
+                                <h4 className="text-1xl"> Best food recipes with the foodie v network </h4>
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-            <>
-            {recipes && (
-                <div className="flex h-full gap-4 rounded-xl p-1 max-md:flex-col min-sm:flex-row">
-                    <Category recipes={recipes} onCategory={onCategory} />
-                    <Recipes recipes={recipesFood} categorys={categorys} ratings={ratings} />
-                </div>
-            )}
+                    <>
+                        {recipes && (
+                            <div className="flex h-full gap-4 rounded-xl p-1 max-md:flex-col min-sm:flex-row">
+                                <Category recipes={recipes} onCategory={onCategory} />
+                                <Recipes recipes={recipesFood} categorys={categorys} ratings={ratings} />
+                            </div>
+                        )}
 
-           
-               <div>
-                    <h4 className="text-3xl font-bold"> Latest Blogs </h4>
-                    {blogs_all.map((blog) => (
-                        <BlogsAll blog={blog} key={blog.id} />
-                    ))}
-              
-            </div>
-          
-</>
-            <Tags tags={tags} onTag={onTag} />
-        </AppLayout>
+                        <div>
+                            <h4 className="text-3xl font-bold"> Latest Blogs </h4>
+                            {blogs_all.map((blog) => (
+                                <BlogsAll blog={blog} key={blog.id} />
+                            ))}
+                        </div>
+                    </>
+                    <Tags tags={tags} onTag={onTag} />
+                </AppLayout>
+            )}
+            {!auth.user && (
+                <>
+                    <Head title="Home" />
+                    <div className="w-full" ref={topRef}>
+                        <h5 className="text-2xl tracking-tight">Your passion for food starts here!</h5>
+
+                        <a href="#" className="flex flex-col max-md:flex-row md:flex-row">
+                            <div className="flex flex-col justify-between px-1 py-4">
+                                <h4 className="text-1xl"> Best food recipes with the foodie v network </h4>
+                            </div>
+                        </a>
+                    </div>
+                    <>
+                        {recipes && (
+                            <div className="flex h-full gap-4 rounded-xl p-1 max-md:flex-col min-sm:flex-row">
+                                <Category recipes={recipes} onCategory={onCategory} />
+                                <Recipes recipes={recipesFood} categorys={categorys} ratings={ratings} />
+                            </div>
+                        )}
+
+                        <div>
+                            <h4 className="text-3xl font-bold"> Latest Blogs </h4>
+                            {blogs_all.map((blog) => (
+                                <BlogsAll blog={blog} key={blog.id} />
+                            ))}
+                        </div>
+                    </>
+                    <Tags tags={tags} onTag={onTag} />
+                    </>
+            )}
+        </>
     );
 }
